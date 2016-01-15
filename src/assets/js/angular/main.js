1f6cd6eecc23eb98c98468885254bfd5b72bff4c
Juan Carlos Orrego <jcorrego@gmail.com>
2016-01-15 14:53:47 -0500

    concerts

--- src/assets/js/angular/main.js
+++ src/assets/js/angular/main.js
@@ -1,10 +1,17 @@
 var blaaApp = angular.module('blaaApp', ['ngSanitize'])
+    .controller('OpusController', ['$scope', '$http', function ($scope, $http) {
+        $http.get('http://blaa.demodayscript.com/conciertos/json').success(function (data) {
+            $scope.concerts = data.nodes;
+        });
+
+    }])
     .controller('MenuController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
         $scope.location = $location;
         $scope.menu = {
             '/': {
                 label: 'Actividad cultural<br>del Banco de la<br>República',
                 title: 'Actividad cultural',
+                subtitle: 'Banco de la República',
                 url: '/',
                 style: 'cIndex'
             }
@@ -12,6 +19,7 @@ var blaaApp = angular.module('blaaApp', ['ngSanitize'])
             '/areas.html': {
                 label: 'Áreas<br>culturales<br>en el país',
                 title: 'Áreas culturales en el país',
+                subtitle: 'Banco de la República',
                 url: '/areas.html',
                 style: 'cAreas'
             }
@@ -19,6 +27,7 @@ var blaaApp = angular.module('blaaApp', ['ngSanitize'])
             '/arte.html': {
                 label: 'Arte y<br>numismática',
                 title: 'Arte y numismática',
+                subtitle: 'Banco de la República',
                 url: '/arte.html',
                 style: 'cArte'
             }
@@ -26,6 +35,7 @@ var blaaApp = angular.module('blaaApp', ['ngSanitize'])
             '/bibliotecas.html': {
                 label: 'Bibliotecas',
                 title: 'Bibliotecas',
+                subtitle: 'Banco de la República',
                 url: '/bibliotecas.html',
                 style: 'cBiblio'
             }
@@ -33,6 +43,7 @@ var blaaApp = angular.module('blaaApp', ['ngSanitize'])
             '/bvirtual.html': {
                 label: 'BVirtual',
                 title: 'BVirtual',
+                subtitle: 'Banco de la República',
                 url: '/bvirtual.html',
                 style: 'cBvirtual'
             }
@@ -40,15 +51,47 @@ var blaaApp = angular.module('blaaApp', ['ngSanitize'])
             '/museo.html': {
                 label: 'Museo<br>del Oro',
                 title: 'Museo del Oro',
+                subtitle: 'Banco de la República',
                 url: '/museo.html',
                 style: 'cMuseo'
             }
             ,
             '/musica.html': {
                 label: 'Música',
-                title: 'Música',
+                title: 'Opus',
+                subtitle: 'Histórico de conciertos del Banco de la República',
                 url: '/musica.html',
-                style: 'cMusica'
+                style: 'cMusica',
+                options: {
+                    '/musica/temporada': {
+                        label: 'Temporada nacional de conciertos',
+                        url:'/musica/temporada'
+                    },
+                    '/musica/programacion': {
+                        label: 'Programación académica',
+                        url:'/musica/programacion'
+                    },
+                    '/musica/jovenes': {
+                        label: 'Convocatoria jóvenes intérpretes',
+                        url:'/musica/jovenes'
+                    },
+                    '/musica/lasala': {
+                        label: 'La sala',
+                        url:'/musica/lasala'
+                    },
+                    '/musica/especiales': {
+                        label: 'Programas especiales',
+                        url:'/musica/especiales'
+                    },
+                    '/musica/boleteria': {
+                        label: 'Boletería',
+                        url:'/musica/boleteria'
+                    },
+                    '/musica/info': {
+                        label: 'Información práctica',
+                        url:'/musica/info'
+                    },
+                }
             }
         }
         ;
